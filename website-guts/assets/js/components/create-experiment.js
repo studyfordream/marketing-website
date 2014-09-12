cf061f74837cf851ceae1d667872a216eb50597a
dtothefp <dtothefp@gmail.com>
2014-09-12 18:36:05 -0400

    add method to modal wrapper

--- website-guts/assets/js/components/create-experiment.js
+++ website-guts/assets/js/components/create-experiment.js
@@ -1,6 +1,17 @@
-// $('#create-exp-form').oForm({
-//   beforeSubmit: function() {
+$('#create-exp-form').on('submit', function() {
+  var inputVals = {},
+    params = '';
 
-
-//   }
-// });
+  $(this).find('input').each(function(i, inputElm) {
+    var inputName = inputElm.getAttribute('name');
+    switch(inputName) {
+      case 'url':
+          params = (inputName + '=' + window.encodeURIComponent( inputElm.value )) + '&' + params;
+        break;
+      case 'name':
+          params += (inputName + '=' + window.encodeURIComponent( inputElm.value ));
+        break;
+    }
+  });
+debugger;  
+});
