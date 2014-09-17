3c5c0037220d68fca562fd335d19ada3452335f4
dtothefp <dtothefp@gmail.com>
2014-09-17 11:52:58 -0400

    update regex for form percent helper

--- website-guts/helpers/omarkdown.js
+++ website-guts/helpers/omarkdown.js
@@ -1,7 +1,6 @@
 var marked = require('optimizely-marked');
 module.exports.register = function (Handlebars)  {
   Handlebars.registerHelper('omarkdown', function (options)  {
-    debugger;
     marked.setOptions({
       linkPath: options.hash.path,
       smartypants: true
