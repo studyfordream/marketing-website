2528a186131019b4b9f54e958ff5f51ea823cd89
Kyle Rush <kylerrush@gmail.com>
2014-09-18 16:08:03 -0400

    Simplify git hook script and remove nvm stuff from setup.sh

--- hooks/check-node-version.sh
+++ hooks/check-node-version.sh
@@ -2,20 +2,6 @@
 
 command -v nvm >/dev/null 2>&1 || { echo >&2 "You must install NVM."; exit 1; }
 
-required_version=`cat .nvmrc`
+source ~/.nvm/nvm.sh
 
-echo "$required_version"
-
-. ~/.nvm/nvm.sh
-
-installed_versions=`nvm list`
-
-echo "$installed_versions"
-
-if [[ ! $installed_versions == *$required_version* ]]
-then
-  echo "You need node v$required_version installed through NVM."
-  exit 1
-fi
-
-nvm use
+nvm use || nvm install
