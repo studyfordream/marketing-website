9b34857e2578a3f43348d2a488379c3878456222
dtothefp <dtothefp@gmail.com>
2014-09-22 17:38:32 -0400

    add validation and param encoding

--- website-guts/assets/js/components/create-experiment.js
+++ website-guts/assets/js/components/create-experiment.js
@@ -1,17 +1,39 @@
-$('#create-exp-form').on('submit', function() {
-  var inputVals = {},
-    params = '';
+$('#create-exp-form').on('submit', function(e) {
+  var uri,
+    params = {},
+    validationError = false,
+    orderQ = ['url'];
+
+  e.preventDefault();
+
+  $(this).find('[required]').each(function(i, elm) {
+    var $elm = $(elm);
+
+    if($elm.val().length === 0) {
+      $('body').addClass('error-state');
+      $elm.parent().find('.' + $elm.attr('name') + '-related').addClass('error-show');
+      $elm.addClass('error-show');
+      validationError = true;
+    }
+  });
+
+  if(validationError) {
+    return;
+  }
 
   $(this).find('input').each(function(i, inputElm) {
     var inputName = inputElm.getAttribute('name');
-    switch(inputName) {
-      case 'url':
-          params = (inputName + '=' + window.encodeURIComponent( inputElm.value )) + '&' + params;
-        break;
-      case 'name':
-          params += (inputName + '=' + window.encodeURIComponent( inputElm.value ));
-        break;
+    if(!!inputElm.value) {
+      params[ inputName ] = inputElm.value;
+      
+      if(orderQ.indexOf(inputName) === -1) {
+        orderQ.push(inputName);
+      }
+
     }
   });
-debugger;  
+
+  uri = window.optly.mrkt.utils.param('/edit', params, orderQ);
+
+  window.location = window.location.origin + uri;
 });
