493e7b3d8c38be86b820124fb2688054b1d1248c
Eric Higgins <ehiggins@us-ehiggins.local>
2014-09-22 14:51:04 -0700

    Cleanup

--- hooks/post-checkout.sh
+++ hooks/post-checkout.sh
@@ -5,7 +5,3 @@ git submodule update --init --force
 
 # "Install" nvm. https://github.com/creationix/nvm#manual-install
 cd .nvm && git checkout `git describe --abbrev=0 --tags` && cd ..
-
-source .nvm/nvm.sh
-
-nvm use || nvm install
