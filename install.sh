87922825751775ac9019a26b4b82ffa768e52c3f
Eric Higgins <ehiggins@us-ehiggins.local>
2014-09-22 14:30:00 -0700

    Install nvm from npm.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Get submodules.
+git submodule update --init --force
+
+# "Install" nvm. https://github.com/creationix/nvm#manual-install
+cd .nvm && git checkout `git describe --abbrev=0 --tags`
