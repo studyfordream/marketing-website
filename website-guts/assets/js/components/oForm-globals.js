c7bc475f135e8923b05095e0dd372c6cea5e2442
Kyle Rush <kylerrush@gmail.com>
2014-09-22 18:06:28 -0400

    Add global create lead function.

--- website-guts/assets/js/components/oForm-globals.js
+++ website-guts/assets/js/components/oForm-globals.js
@@ -38,4 +38,54 @@
 
   };
 
+  w.optly.mrkt.Oform.trackLead = function(data, XMLHttpRequest){
+
+    var propertyName,
+        reportingObject,
+        source;
+
+    source = w.optly.mrkt.source;
+
+    reportingObject = {
+
+      token: JSON.parse(XMLHttpRequest.target.responseText) || '',
+      utm_Campaign__c: source.utm.campaign,
+      utm_Content__c: source.utm.content,
+      utm_Medium__c: source.utm.medium,
+      utm_Source__c: source.utm.source,
+      utm_Keyword__c: source.utm.keyword,
+      otm_Campaign__c: source.otm.campaign,
+      otm_Content__c: source.otm.content,
+      otm_Medium__c: source.otm.medium,
+      otm_Source__c: source.otm.source,
+      otm_Keyword__c: source.otm.keyword,
+      GCLID__c: source.gclid,
+      Signup_Platform__c: source.signupPlatform
+
+    };
+
+    for(propertyName in data){
+
+      reportingObject['propertyName'] = data['propertyName'];
+
+    }
+
+    w.analytics.identify(email, reportingObject, {
+      integrations: {
+        Marketo: true
+      }
+    });
+    w.analytics.track('account created', {
+      category: 'account',
+      label: w.location.pathname
+    }, {
+      Marketo: true
+    });
+    w.analytics.track('account signin', {
+      category: 'account',
+      lable: w.location.pathname
+    });
+
+  };
+
 })();
