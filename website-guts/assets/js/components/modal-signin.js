de27989b30daaaf656deac70f4141bd502f04b81
dtothefp <dtothefp@gmail.com>
2014-09-24 16:51:57 -0400

    remove use strict closure from signin

--- website-guts/assets/js/components/modal-signin.js
+++ website-guts/assets/js/components/modal-signin.js
@@ -1,22 +1,16 @@
-(function(){
-  'use strict';
+var signinDialogHelperInst = window.optly.mrkt.form.signin('signin-dialog');
 
-  var signinDialogHelperInst = window.optly.mrkt.form.signin('signin-dialog');
-
-  var signinForm = new Oform({
-    selector: '#signin-form',
-    customValidation: {
-      password: function(elm) {
-        return signinDialogHelperInst.passwordValidation(elm);
-      }
+var signinForm = new Oform({
+  selector: '#signin-form',
+  customValidation: {
+    password: function(elm) {
+      return signinDialogHelperInst.passwordValidation(elm);
     }
-  });
+  }
+});
 
-  signinForm.on('validationerror', w.optly.mrkt.Oform.validationError);
+signinForm.on('validationerror', w.optly.mrkt.Oform.validationError);
 
-  signinForm.on('load', signinDialogHelperInst.load.bind(signinDialogHelperInst));
+signinForm.on('load', signinDialogHelperInst.load.bind(signinDialogHelperInst));
 
-  signinForm.on('done', w.optly.mrkt.Oform.done);
-
-
-}());
+signinForm.on('done', w.optly.mrkt.Oform.done);
