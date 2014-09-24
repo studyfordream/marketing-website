2eb417314f570a86719a45d028158d40917df75d
Kyle Rush <kylerrush@gmail.com>
2014-09-24 02:08:57 -0400

    oform for /demo.

--- website-guts/assets/js/layouts/benefits.js
+++ website-guts/assets/js/layouts/benefits.js
@@ -25,39 +25,4 @@ window.optly.mrkt.benefitsLayout.menuOrder = function() {
 window.optly.mrkt.benefitsLayout.menuHandlers();
 window.optly.mrkt.benefitsLayout.menuOrder();
 
-new Oform({
-  selector: '#contact-form'
-})
-.on('validationerror', w.optly.mrkt.Oform.validationError)
-.on('load', function(event){
-  if(event.target.status === 200){
-    //identify user
-    $('body').addClass('oform-success');
-    var response = JSON.parse(event.target.responseText),
-        email = d.querySelector('[name="email"]').value,
-        traffic = d.querySelector('#traffic');
-    w.analytics.identify(email, {
-      name: d.querySelector('[name="name"]').value,
-      email: email,
-      phone: d.querySelector('[name="phone"]').value || '',
-      company: d.querySelector('[name="company"]').value || '',
-      website: d.querySelector('[name="website"]').value || '',
-      utm_Medium__c: window.optly.mrkt.source.utm.medium,
-      otm_Medium__c: window.optly.mrkt.source.otm.medium,
-      Demo_Request_Monthly_Traffic__c: traffic.options[traffic.selectedIndex].value || '',
-      Inbound_Lead_Form_Type__c: d.querySelector('[name="inboundFormLeadType"]').value,
-      token: response.token
-    }, {
-      integrations: {
-        Marketo: true
-      }
-    });
-    //track the event
-    w.analytics.track('demo requested', {
-      category: 'contact form',
-      label: w.location.pathname
-    }, {
-      Marketo: true
-    });
-  }
-});
+w.optly.mrkt.Oform.initContactForm({selector: '#contact-form'});
