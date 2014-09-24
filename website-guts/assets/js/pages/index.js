a3276024ba62811f17fed90e906b475a20a62abd
Kyle Rush <kylerrush@gmail.com>
2014-09-24 00:34:52 -0400

    updated.

--- website-guts/assets/js/pages/index.js
+++ website-guts/assets/js/pages/index.js
@@ -27,66 +27,6 @@ $('#test-it-out-form').submit(function(e){
 
 });
 
-$('#touch-homepage-create-account-form').oForm({
-
-  validation: {
-
-    password1: window.optly.mrkt.oForm.validatePassword,
-    password2: function(){
-
-      return $('#touch-cta input[name="password1"]').val() === $('#touch-cta input[name="password2"]').val();
-
-    }
-
-  },
-  afterLocal: function(rs, cb){
-
-    //window.alert('heyo!');
-    //console.log('afterLocal running');
-
-    if(typeof rs === 'object'){
-
-      console.log('rs: ', + rs);
-
-      var r = rs.responeJSON;
-
-      console.log('r: ', r);
-
-      window.analytics.identify(r.email, {
-        name: r.first_name + ' ' + r.last_name,
-        email: r.email,
-        plan: r.subscription_plan
-      });
-
-      window.analytics.track('/account/create/success', {
-        category: 'Accounts',
-        label: window.location.pathname
-      },{
-        Marketo: true
-      });
-
-      window.analytics.track('/account/signin', {
-        category: 'Accounts',
-        label: window.location.pathname
-      },{
-        Marketo: true
-      });
-
-      //to do: execute sign in function
-
-      //run callback
-      if(typeof cb === 'function'){
-
-        cb(rs);
-
-      }
-
-    } else if(typeof cb === 'function'){
-
-      cb();
-
-    }
-
-  }
-
+new Oform({
+  selector: '#touch-homepage-create-account-form'
 });
