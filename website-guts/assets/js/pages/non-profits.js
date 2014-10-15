9faa3ecc9c206ba841c5d40bc32cf68414d71274
dtothefp <dtothefp@gmail.com>
2014-10-14 19:48:32 -0700

    add oform

--- website-guts/assets/js/pages/non-profits.js
+++ website-guts/assets/js/pages/non-profits.js
@@ -1,5 +1,46 @@
+function animBtnColor(button) {
+  var btnClass = button.classList;
+  if(btnClass.contains('link-hover-color')) {
+    btnClass.remove('link-hover-color');
+  } else {
+    btnClass.add('link-hover-color');
+  }
+}
+
+function slide($button) {
+  $.each($button, function(i, button) {
+    $(button).on(window.optly.mrkt.anim.transitionend, function() {
+      animBtnColor(button.firstElementChild);
+    });
+  });
+}
+
+
 $(function(){
   if(!window.optly.mrkt.isMobile()) {
     window.skrollr.init();
   }
+  
+  var orgFormHelperInst = window.optly.mrkt.form.orgForm({formId: 'org-form'});
+  
+  var orgForm = new Oform({
+    selector: '#org-form'
+  });
+
+  orgForm.on('validationerror', w.optly.mrkt.Oform.validationError);
+
+  orgForm.on('error', function() {
+    orgFormHelperInst.processingRemove({callee: 'error'});
+    window.analytics.track('signin xhr error', {
+      category: 'account',
+      label: w.location.pathname
+    });
+  }.bind());
+
+  orgForm.on('load', orgFormHelperInst.bind(orgFormHelperInst));
+
+  orgForm.on('done', function(){
+    orgFormHelperInst.processingRemove({callee: 'done'});
+  }.bind(orgFormHelperInst));
+
 });
---
+++ website-guts/assets/js/utils/form_helpers/non_profits_helpers.js
@@ -0,0 +1,38 @@
+window.optly.mrkt.form = window.optly.mrkt.form || {};
+
+var oformHelper = {
+  
+  showOptionsError: function (message){
+    if(!document.body.classList.contains('error-state')) {
+      document.body.classList.add('error-state');
+    }
+    if( !this.optionsErrorElm.classList.contains('error-show') ) {
+      this.optionsErrorElm.classList.add('error-show');
+    }
+    this.optionsErrorElm.innerHTML = message;
+  },
+
+  load: function(e) {
+    var resp = JSON.parse(e.target.responseText);
+debugger;
+    if(e.target.status !== 200) {
+      this.processingRemove({callee: 'load'});
+      this.showOptionsError(resp.error);
+      return;
+    }
+
+    this.processingRemove({callee: 'load', retainDisabled: true});
+
+  }
+};
+
+window.optly.mrkt.form.orgForm = function(argumentsObj) {
+  var constructorArgs = {
+    formId: argumentsObj.formId,
+    prototype: oformHelper
+  };
+
+  return window.optly.mrkt.form.HelperFactory(constructorArgs);
+
+};
+
