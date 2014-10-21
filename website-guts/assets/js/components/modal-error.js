4ea3514669a9475a69ffcad856f28cc94cb843de
dtothefp <dtothefp@gmail.com>
2014-10-20 23:10:44 -0700

    create Q for logging dialog errors in user service

--- website-guts/assets/js/components/modal-error.js
+++ website-guts/assets/js/components/modal-error.js
@@ -1,29 +1,57 @@
 var DEFAULT_ERROR_MESSAGE = 'Don\'t worry, most issues are minor - please refresh your browser and try again.';
+var errorShown;
 
 function showError(errorMessage, errorId) {
   var info = {
-    errorMessage: errorMessage || DEFAULT_ERROR_MESSAGE,
-    guid: errorId || window.optly.mrkt.utils.generateGuid(),
-    timestamp: window.optly.mrkt.utils.formatDateJson(new Date())
-  };
+      errorMessage: errorMessage || DEFAULT_ERROR_MESSAGE,
+      guid: errorId || window.optly.mrkt.utils.generateGuid(),
+      timestamp: window.optly.mrkt.utils.formatDateJson(new Date())
+    },
+    $errorMsgElm = $('<span>').text(info.errorMessage),
+    $errorIdElm = $('<span>').text(info.guid + ' | ' + info.timestamp);
 
   //TODO if no errorId submit generated GUID to splunk
   //https://github.com/optimizely/optimizely/blob/effa021dfcebdb49ae74ed0982ed0cb99493dbb7/src/www/js/bundle/page.js#L177
   //https://github.com/optimizely/optimizely/blob/devel/src/www/js/bundle/common/errors.js#L90
   $('.optimizely_error').hide();
-  $('#optimizely_error_dialog #optimizely_error_type').text(info.errorMessage);
-  window.optly.mrkt.modal.open({ modalType: 'error' });
-  $('#optimizely_error_dialog #optimizely_error_info').text(info.guid + ' | ' + info.timestamp);
+  $('#optimizely_error_dialog #optimizely_error_type').append($errorMsgElm);
+  $('#optimizely_error_dialog #optimizely_error_info').append($errorIdElm);
+
+  if(!errorShown) {
+    window.optly.mrkt.modal.open({ modalType: 'error' });
+    errorShown = true;
+  }
+  
   return info;
 }
 
-function checkUrlError () {
+function checkError() {
   var queryParameters = window.optly.mrkt.utils.deparam(window.location.href);
   if (Object.keys(queryParameters).indexOf('error') !== -1) {
     showError(queryParameters.error, queryParameters.error_id);
   }
 }
 
+function logError(errorObj) {
+  showError(errorObj.error, errorObj.error_id);
+}
+
+function ErrorQ(fnCache) {
+  this.fnCache = fnCache;
+}
+
+ErrorQ.prototype = window.optly.mrkt.Optly_Q.prototype;
+
+var oldErrorQ = window.optly.mrkt.errorQ;
+
+var errorQ = new ErrorQ({logError: logError});
+
 $(function() {
-  checkUrlError();
+
+  checkError();
+
+  if(oldErrorQ.length !== 0) {
+    errorQ.push(oldErrorQ);
+  }
+  
 });
--- website-guts/assets/js/services/user_state.js
+++ website-guts/assets/js/services/user_state.js
@@ -2,26 +2,7 @@ window.optly                = window.optly || {};
 window.optly.mrkt           = window.optly.mrkt || {};
 window.optly.mrkt.services  = window.optly.mrkt.services || {};
 window.optly.mrkt.user      = window.optly.mrkt.user || {};
-
-var initSegmentQ = function() {
-  window.analytics = window.analytics || [];
-
-  window.analytics.methods = ['identify', 'group', 'track', 'page', 'pageview', 'alias', 'ready', 'on', 'once', 'off', 'trackLink', 'trackForm', 'trackClick', 'trackSubmit'];
-
-  window.analytics.factory = function(method){
-    return function(){
-      var args = Array.prototype.slice.call(arguments);
-      args.unshift(method);
-      window.analytics.push(args);
-      return window.analytics;
-    };
-  };
-
-  for (var i = 0; i < window.analytics.methods.length; i++) {
-    var key = window.analytics.methods[i];
-    window.analytics[key] = window.analytics.factory(key);
-  }
-};
+window.optly.mrkt.errorQ    = window.optly.mrkt.errorQ || [];
 
 window.optly.mrkt.Optly_Q = function(acctData, expData){
   var objectCreateSupported = typeof Object.create === 'function';
@@ -85,12 +66,12 @@ window.optly.mrkt.Optly_Q = function(acctData, expData){
 
 window.optly.mrkt.Optly_Q.prototype = {
 
-  transformQuedArgs: function(quedArgs) {
+  transformQueuedArgs: function(queuedArgs) {
     var funcArgs = [];
-    $.each(quedArgs, function(index, arg) {
-      if (this[ arg ] !== undefined && arg !== 'object') {
+    $.each(queuedArgs, function(index, arg) {
+      if (this[ arg ] !== undefined && typeof arg !== 'object') {
         funcArgs.push(this[ arg ]);
-      } else if(arg === 'object') {
+      } else if(typeof arg === 'object') {
         funcArgs.push(arg);
       }
     }.bind(this));
@@ -98,44 +79,45 @@ window.optly.mrkt.Optly_Q.prototype = {
     return funcArgs;
   },
 
-  parseQ: function(fnQ, i) {
-    var quedArgs, transformedArgs;
-    //if not a nested array
-    if (typeof fnQ[i] === 'function') {
-      quedArgs = fnQ.slice(1);
+  parseQ: function(fnQ) {
+    var queuedArgs, 
+      transformedArgs,
+      queuedFn = fnQ[0];
 
-      //if there are no arguments call the function with the object scope
-      if(quedArgs.length === 0) {
-        fnQ[i].call(this);
-      } else {
-        transformedArgs = this.transformQuedArgs(quedArgs);
-        fnQ[i].apply( fnQ[i], transformedArgs );
-      }
+    queuedArgs = fnQ.slice(1);
 
+    //if there are no arguments call the function with the object scope
+    if(queuedArgs.length === 0) {
+      queuedFn.call(this);
+    } else {
+      transformedArgs = this.transformQueuedArgs(queuedArgs);
+      queuedFn.apply( queuedFn, transformedArgs );
     }
-    //if a nested array
-    else {
-      for(var nestedI = 0; nestedI < fnQ[i].length; nestedI += 1) {
 
-        if (typeof fnQ[i][nestedI] === 'function') {
-          quedArgs = fnQ[i].slice(1);
-
-          //if there are no arguments call the function with the object scope
-          if(quedArgs.length === 0) {
-            fnQ[i][nestedI].call(this);
-          } else {
-            transformedArgs = this.transformQuedArgs(quedArgs);
-            fnQ[i][nestedI].apply( fnQ[i][nestedI], transformedArgs );
-          }
-
-        }
-      }
-    }
   },
 
   push: function(fnQ) {
-    for (var i = 0; i < fnQ.length; i += 1) {
-      this.parseQ(fnQ, i);
+
+    switch(typeof fnQ[0]) {
+      case 'function':
+        this.parseQ(fnQ);
+      break;
+
+      case 'string':
+        fnQ[0] = this.fnCache[ fnQ[0] ];
+        this.parseQ(fnQ);
+      break;
+
+      default:
+        for (var i = 0; i < fnQ.length; i += 1) {
+          if(typeof fnQ[i][0] === 'function') {
+            this.parseQ(fnQ[i]);
+          } else if(typeof fnQ[i][0] === 'string') {
+            fnQ[i][0] = this.fnCache[ fnQ[i][0] ];
+            this.parseQ(fnQ[i]);
+          }
+        }
+      break;
     }
   }
 
@@ -249,13 +231,18 @@ window.optly.mrkt.services.xhr = {
 
           //parse JSON and catch any errors -- if error return immediately
           try {
-            parsedRes = $.parseJSON(jqXHR.responseText);
+            parsedRes = JSON.parse(jqXHR.responseText);
           } catch (error) {
+              window.optly.mrkt.errorQ.push([
+                'logError',
+                {
+                  error: 'Error Parsing the Response of Your Account Data',
+                }
+              ]);
+              this.logSegmentError(url, 'api error', 'response contains invalid json ' + error);
 
-            this.logSegmentError(url, 'api error', 'response contains invalid json ' + error);
-
-            // do not check validations if parse error
-            return undefined;
+              // do not check validations if parse error
+              return undefined;
           }
 
           // validate each property type
@@ -265,18 +252,33 @@ window.optly.mrkt.services.xhr = {
         // if the http request fails the jqXHR object will not be promise
         else {
           // in this case the data object is a promise so we parse it's response text
-          if ( this.isPromise(data) && data.status === 200) {
+          if ( this.isPromise(data) ) {
             try {
-              parsedRes = $.parseJSON(data.responseText);
+              parsedRes = JSON.parse(data.responseText);
             } catch (error) {
-              errorMessage = error + ', Response Text: ' + data.responseText + ', Status Text: ' + data.statusText + ', Status: ' + data.status;
+                errorMessage = error + ', Response Text: ' + data.responseText + ', Status Text: ' + data.statusText + ', Status: ' + data.status;
+                window.optly.mrkt.errorQ.push([
+                  'logError',
+                  {
+                    error: 'Error Parsing the Response of Your Account Data',
+                  }
+                ]);
             }
-          }
-          if (errorMessage === undefined) {
-            errorMessage = 'Response Text: ' + data.responseText + ', Status Text: ' + data.statusText + ', Status: ' + data.status;
-          }
+          
+            if (errorMessage === undefined && data.status !== 200) {
+              errorMessage = 'Response Text: ' + data.responseText + ', Status Text: ' + data.statusText + ', Status: ' + data.status;
+              window.optly.mrkt.errorQ.push([
+                'logError',
+                {
+                error: JSON.parse(data.responseText).error,
+                'error_id': JSON.parse(data.responseText).id
+                }
+              ]);
+            }
 
-          this.logSegmentError(url, 'api error', errorMessage);
+            this.logSegmentError(url, 'api error', errorMessage);
+
+          }
 
         }
 
@@ -307,7 +309,6 @@ window.optly.mrkt.services.xhr = {
         if (index === tranformedArgs.length - 1) {
           oldQue = window.optly_q;
 
-          // consume qued data
           window.optly_q = window.optly.mrkt.Optly_Q(responses[0], responses[1]);
           window.optly_q.push(oldQue);
         }
@@ -368,6 +369,5 @@ window.optly.mrkt.services.xhr = {
     }
   };
 
-  initSegmentQ();
   window.optly.mrkt.services.xhr.getLoginStatus([acctParams, expParams]);
 }());
