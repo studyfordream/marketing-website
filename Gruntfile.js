0682bc01b978027eeee11b79eae66e96047dd6be
dtothefp <dtothefp@gmail.com>
2014-10-29 17:28:13 -0400

    remove comments from grunt task script

--- Gruntfile.js
+++ Gruntfile.js
@@ -914,7 +914,7 @@ module.exports = function(grunt) {
     'replace',
     'autoprefixer',
     'copy',
-    //'clean:postBuild',
+    'clean:postBuild',
     'connect:livereload',
     'watch'
   ]);
