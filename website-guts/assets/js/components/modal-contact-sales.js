30f473a967f1d6f6b4125e9f4b3b8d67812cb5ac
kylerush <kylerrush@gmail.com>
2014-11-11 11:26:26 -0500

    update

---
+++ website-guts/assets/js/components/modal-contact-sales.js
@@ -0,0 +1,22 @@
+//make this global in case someone needs to remove the Oform instance
+w.optly.mrkt.activeModals.contactSales = new Oform({
+
+  selector: 'form#contact-sales'
+
+});
+
+var contactSalesForm = w.optly.mrkt.activeModals.contactSales;
+
+contactSalesForm.on('before', function(){
+
+
+
+}).on('validationError', function(){
+
+  
+
+}).on('load', function(event){
+
+
+
+});
