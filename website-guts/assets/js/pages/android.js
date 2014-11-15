daa6a44acd225cb678db50a413b2eb2dc6992039
dtothefp <dtothefp@gmail.com>
2014-11-14 19:09:29 -0500

    add form helpers and refactor css to min width

--- website-guts/assets/js/pages/android.js
+++ website-guts/assets/js/pages/android.js
@@ -6,52 +6,28 @@
 $(function() {
   //deal with input icon animation
   window.optly.mrkt.anim.placeholderIcons({inputs: $('.android__content input[type="email"]')});
-});
+  
+  //create the helper instance
+  var androidFormHelperInst = window.optly.mrkt.form.android({formId: 'android-form'});
 
-new Oform({
-  selector: '.android__content form'
-})
-.on('validationerror', function( element ) {
-  var errorMessage = $(element).data('error');
-  $(element).parents('fieldset').after('<p class="oform-error-message">' + errorMessage + '</p>');
-})
-.on('error', function(e) {
-})
-.on('success', function(formData) {
-
-  $('.android__content').each(function() {
-    var $self = $(this);
-    if ( $self.hasClass('is-visible') ) {
-      $self.addClass('is-hidden').removeClass('is-visible');
-    } else {
-      $self.addClass('is-visible').removeClass('is-hidden');
+  new Oform({
+    selector: '#android-form'
+  })
+  .on('before', function() {
+    androidFormHelperInst.processingAdd();
+    androidFormHelperInst.removeErrors();
+  })
+  .on('validationerror', function(elm) {
+    w.optly.mrkt.Oform.validationError(elm);
+  })
+  .on('success', function() {
+    androidFormHelperInst.success($('.android__content'));
+  })
+  .on('done', function() {
+    if(document.body.classList.contains('oform-error')) {
+      androidFormHelperInst.processingRemove({callee: 'done'});
+      androidFormHelperInst.showOptionsError();
     }
   });
 
-  //tracking code goes here
-  w.analytics.identify(formData.data.email, {
-
-    Inbound_Lead_Form_Type__c: 'Android Developer Preview',
-    LeadSource: 'Website',
-    LeadSourceCategory: 'Content',
-    LeadSourceSubcategory: 'PMM',
-    email: formData.data.email
-
-  }, {
-
-    Integrations: {
-      Marketo: true
-    }
-
-  });
-
-  w.Munchkin.munchkinFunction('visitWebPage', {
-    url: '/event/form-submit-android-preview'
-  });
-
-  w.analtyics.track('android preview success', {
-
-    category: 'forms'
-
-  });
 });
