31e9f2d71782088b9a61d5fa5ca265f1d0dcaa1c
kylerush <kylerrush@gmail.com>
2014-12-18 20:53:17 -0500

    updated

--- website-guts/assets/js/ppc/ppc.js
+++ website-guts/assets/js/ppc/ppc.js
@@ -2,7 +2,27 @@ if(Modernizr.placeholder){
   w.optly.mrkt.inlineFormLabels();
 }
 
+if(!Modernizr.touch){
+  $('#url').focus();
+}
+
 d.querySelector('[name="hidden"]').value = 'touched';
+
+//track focus on form fields
+$('#seo-form input:not([type="hidden"])').each(function(){
+  $(this).one('blur', function(){
+    //put all the information in the event because we'll want to use this as a goal in optimizely
+    w.analytics.track($(this).closest('form').attr('id') + ' ' + $(this).attr('name') + ' focus',
+    {
+      category: 'forms'
+    },
+    {
+      integrations: {
+        'Marketo': false
+      }
+    });
+  });
+});
 
 w.optly.mrkt.trialForm = new Oform({
   selector: '#seo-form',
