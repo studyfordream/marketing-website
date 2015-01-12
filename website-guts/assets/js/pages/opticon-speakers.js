1ecde16f08e8ecd0dd5759aff359589b14be43cd
Stewart Smith <stewart@optimizely.com>
2015-01-12 10:37:54 -0800

    address feedback for layout, styling, and images

--- website-guts/assets/js/pages/opticon-speakers.js
+++
@@ -1,8 +0,0 @@
-$('.form-success').hide();
-
-var opticonSpeakerHelper = window.optly.mrkt.form.opticonSpeakerForm({formId: 'speaker-form'});
-
-$(opticonSpeakerHelper.formElm).on('submit', function(e) {
-  e.preventDefault();
-  this.validateForm();
-}.bind(opticonSpeakerHelper));
