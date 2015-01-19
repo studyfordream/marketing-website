ec04a94dd43d37bf324113577e43816bab615e5a
Cameron Corda <ccorda@gmail.com>
2015-01-18 16:29:12 -0800

    Moving script inline to get past undefined `fn_vidyard_Bf0qpT872TFpgGnJUqm0kA();` function.

--- website-guts/assets/js/pages/statistics.js
+++ website-guts/assets/js/pages/statistics.js
@@ -10,19 +10,7 @@ function smoothScroll(e) {
 
 }
 
-/*
-* Video Player
-*
-*/
-var player;
 $(function() {
-
-  //video player open and autoplay
-  $('[data-show-video]').on('click', function(e) {
-    e.preventDefault();
-    fn_vidyard_Bf0qpT872TFpgGnJUqm0kA();
-  });
-
   //listen for smooth scrolling
   $('[smooth-scroll]').on('click', smoothScroll);
 });
