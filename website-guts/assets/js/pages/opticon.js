973083c075434233c0ad0db196e556482ed1b7ca
Stewart Smith <stewart@optimizely.com>
2015-01-21 14:29:33 -0500

    use param and deparam functions

--- website-guts/assets/js/pages/opticon.js
+++ website-guts/assets/js/pages/opticon.js
@@ -1,3 +1,5 @@
-if (window.optly.mrkt.utils.getURLParameter('aff') === 'butterbar') {
-  $('#register .cta').attr('href', 'https://opticon2015.eventbrite.com/?ref=elink&aff=butterbar');
+var urlParams = window.optly.mrkt.utils.deparam(window.location.search);
+if (urlParams) {
+  var replacementHref = window.optly.mrkt.utils.param('https://opticon2015.eventbrite.com/?ref=elink', urlParams);
+  $('#register .cta').attr('href', replacementHref);
 }
