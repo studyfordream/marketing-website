b68c27cc011b3ddf99d5007e85e2b2edf7078e97
David Fox-Powell <dtothefp@gmail.com>
2015-01-22 15:45:50 -0500

    add stringify function for error object

---
+++ website-guts/assets/js/utils/stringify_error.js
@@ -0,0 +1,13 @@
+//This extends the Error object, it's in utils just to keep track of it by name
+Object.defineProperty(Error.prototype, 'toJSON', {
+    value: function () {
+        var alt = {};
+
+        Object.getOwnPropertyNames(this).forEach(function (key) {
+            alt[key] = this[key];
+        }, this);
+
+        return alt;
+    },
+    configurable: true
+});
