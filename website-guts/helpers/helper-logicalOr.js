bf1e22e5c6beb8b93aedc9a1ef75e7183b4b5d07
Stewart Smith <stewart@optimizely.com>
2015-01-30 10:23:31 -0500

    include ad scripts on every page

--- website-guts/helpers/helper-logicalOr.js
+++
@@ -1,8 +0,0 @@
-module.exports.register = function (Handlebars)  {
-  Handlebars.registerHelper('logicalOr', function(v1, v2, options) {
-    if (v1 || v2) {
-      return options.fn(this);
-    }
-    return options.inverse(this);
-  });
-};
