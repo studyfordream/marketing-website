cd2ace42c72d4bcbe61f2799e40fbd1959387a42
Stewart Smith <stewart@optimizely.com>
2015-02-04 12:46:21 -0800

    add segment reporting under an email regex check

--- website-guts/assets/js/pages/homepage.js
+++ website-guts/assets/js/pages/homepage.js
@@ -8,6 +8,17 @@ $('#get-started').submit(function(e){
     w.optly.mrkt.modal.open({ modalType: 'signup' });
     d.body.classList.add('test-it-out-success');
     $('input[type="email"]').val(inputVal);
+
+    var emailRegEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailRegEx.test(inputVal)) {
+      var anonymousVisitorIdentifier = window.optly.mrkt.utils.randomString();
+      w.analytics.identify(
+        anonymousVisitorIdentifier,
+        { email: inputVal },
+        { integrations: { Marketo: true } }
+      );
+    }
+
   } else {
     $('input[type="email"]').focus();
   }
