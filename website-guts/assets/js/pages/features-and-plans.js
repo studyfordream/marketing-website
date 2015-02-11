19fdc470a188515efc67fb4313e6352508e8ba09
Stewart Smith <stewart@optimizely.com>
2015-02-11 13:50:36 -0500

    remove extra click handler

--- website-guts/assets/js/pages/features-and-plans.js
+++ website-guts/assets/js/pages/features-and-plans.js
@@ -8,11 +8,6 @@ $('#enterprise-cta').on('click', function(e){
   w.optly.mrkt.modal.open({ modalType: 'contact-sales' });
 });
 
-$('#starter-cta').on('click', function(e){
-  e.preventDefault();
-  w.optly.mrkt.modal.open({ modalType: 'signup' });
-});
-
 $('#learn-cta').on('click', w.optly.mrkt.utils.smoothScroll);
 $('#plans-cta').on('click', w.optly.mrkt.utils.smoothScroll);
 
