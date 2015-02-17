a4687247b7c6d04fb67cac24be31e23325c8167f
Stewart Smith <stewart@optimizely.com>
2015-02-17 17:31:58 -0500

    create seo form helper and use that in form

--- website-guts/assets/js/layouts/seo.js
+++ website-guts/assets/js/layouts/seo.js
@@ -2,8 +2,6 @@ $('[name="hidden"]').val('touched');
 
 document.querySelector('[name="hidden"]').value = 'touched';
 
-var xhrInitiationTime;
-
 //track focus on form fields
 $('#seo-form input:not([type="hidden"])').each(function(){
   $(this).focus(function(){
@@ -18,79 +16,20 @@ $('#seo-form input:not([type="hidden"])').each(function(){
   });
 });
 
-//form
+var seoFormHelperInst = window.optly.mrkt.form.seoForm({formId: 'seo-form'});
+
 new Oform({
   selector: '#seo-form'
 })
 .on('before', function(){
-  w.analytics.track('/free-trial/submit', {
-    category: 'account',
-    label: w.optly.mrkt.utils.trimTrailingSlash(w.location.pathname)
-  }, {
-    integrations: {
-      'Marketo': false
-    }
-  });
-  xhrInitiationTime = new Date();
-  return w.optly.mrkt.Oform.before();
+  seoFormHelperInst.before();
 })
 .on('validationerror', w.optly.mrkt.Oform.validationError)
 .on('error', function(){
   $('#seo-form .error-message').text('An unknown error occured.');
   $('body').addClass('oform-error').removeClass('oform-processing');
 })
-.on('load', function(event){
-  var xhrElapsedTime = new Date() - xhrInitiationTime,
-      response = this.parseResponse(event);
-
-  if(response){
-    //remove error class from body?
-    w.optly.mrkt.Oform.trackLead({
-      email: d.getElementById('email').value,
-      url: d.getElementById('url').value,
-      name: d.getElementById('name').value,
-      phone: d.getElementById('phone').value
-    }, event);
-    //[> legacy reporting - to be deprecated <]
-    w.analytics.track('/free-trial/success', {
-      category: 'account',
-      label: w.location.pathname
-    }, {
-      'Marketo': false
-    });
-    w.Munchkin.munchkinFunction('visitWebPage', {
-      url: '/free-trial/success'
-    });
-    w.analytics.page('/account/create/success', {
-      integrations: {
-        'Marketo': false
-      }
-    });
-    w.analytics.page('/free-trial/success', {
-      integrations: {
-        'Marketo': false
-      }
-    });
-
-    //for phantom tests
-    document.body.dataset.formSuccess = document.getElementById('seo-form').getAttribute('action');
-
-    setTimeout(function(){
-      w.location = 'https://www.optimizely.com/welcome';
-    }, 1000);
-  }
-})
+.on('load', seoFormHelperInst.success.bind(seoFormHelperInst))
 .on('done', function(){
-  if($('body').hasClass('oform-error')){
-    $('body').removeClass('oform-processing');
-    //report that there were errors in the form
-    w.analytics.track('seo-form validation error', {
-      category: 'form error',
-      label: $('input.oform-error-show').length + ' errors',
-    }, {
-      integrations: {
-        'Marketo': false
-      }
-    });
-  }
+  seoFormHelperInst.done();
 });
---
+++ website-guts/assets/js/utils/form_helpers/seo_helpers.js
@@ -0,0 +1,108 @@
+window.optly.mrkt.form = window.optly.mrkt.form || {};
+
+var seoHelper = {
+
+  before: function() {
+    w.analytics.track('/free-trial/submit', {
+      category: 'account',
+      label: w.optly.mrkt.utils.trimTrailingSlash(w.location.pathname)
+    }, {
+      integrations: {
+        'Marketo': false
+      }
+    });
+    return w.optly.mrkt.Oform.before();
+  },
+
+  success: function(event) {
+    var response = this.parseResponse(event);
+
+    if(response){
+      //remove error class from body?
+      w.optly.mrkt.Oform.trackLead({
+        email: d.getElementById('email').value,
+        url: d.getElementById('url').value,
+        name: d.getElementById('name').value,
+        phone: d.getElementById('phone').value
+      }, event);
+      //[> legacy reporting - to be deprecated <]
+      w.analytics.track('/free-trial/success', {
+        category: 'account',
+        label: w.location.pathname
+      }, {
+        'Marketo': false
+      });
+      w.Munchkin.munchkinFunction('visitWebPage', {
+        url: '/free-trial/success'
+      });
+      w.analytics.page('/account/create/success', {
+        integrations: {
+          'Marketo': false
+        }
+      });
+      w.analytics.page('/free-trial/success', {
+        integrations: {
+          'Marketo': false
+        }
+      });
+
+      this.redirectHelper({
+        redirectPath: w.linkPath + '/welcome',
+        bodyData: {
+          formSuccess: document.getElementById('seo-form').getAttribute('action')
+        }
+      });
+    } else {
+      w.analytics.track(w.optly.mrkt.utils.trimTrailingSlash(w.location.pathname), {
+        category: 'api error',
+        label: 'status not 200: ' + event.target.status
+      }, {
+        integrations: {
+          'Marketo': false
+        }
+      });
+
+      if(response.error && typeof response.error === 'string'){
+        //update error message, apply error class to body
+        $('#seo-form .error-message').text(response.error);
+        $('body').addClass('oform-error').removeClass('oform-processing');
+        w.analytics.track(w.optly.mrkt.utils.trimTrailingSlash(w.location.pathname), {
+          category: 'api error',
+          label: 'response.error: ' + response.error
+        }, {
+          integrations: {
+            'Marketo': false
+          }
+        });
+      } else {
+        this.showOptionsError({error: 'UNEXPECTED'});
+        $('body').addClass('oform-error').removeClass('oform-processing');
+      }
+    }
+  },
+
+  done: function() {
+    if($('body').hasClass('oform-error')){
+      $('body').removeClass('oform-processing');
+      //report that there were errors in the form
+      w.analytics.track('seo-form validation error', {
+        category: 'form error',
+        label: $('input.oform-error-show').length + ' errors',
+      }, {
+        integrations: {
+          'Marketo': false
+        }
+      });
+    }
+  }
+};
+
+window.optly.mrkt.form.seoForm = function(argumentsObj) {
+  var constructorArgs = {
+    formId: argumentsObj.formId,
+    prototype: seoHelper
+  };
+
+  return window.optly.mrkt.form.HelperFactory(constructorArgs);
+
+};
