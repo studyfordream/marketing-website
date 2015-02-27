7db7117f1c281e6d2eaab6aed2fcbcf9a3717916
David Fox-Powell <dtothefp@gmail.com>
2015-02-27 14:28:59 -0500

    update dictionary function to account for arrays and null values

--- grunt/assemble/utils/create-dictionary.js
+++ grunt/assemble/utils/create-dictionary.js
@@ -4,22 +4,31 @@ var marked = require('optimizely-marked');
 //need to have a way to recognize markdown in page content in hbs template outside of front matter
 //when putting data back in must recognize arrays and sub out strings in objects accordingly
 module.exports = function (assemble) {
-  var linkPath = assemble.get('data.linkPath');
+  var locales = assemble.get('data.locales');
 
-  function parseMarkdown(mdStr, MDparser) {
-    return MDparser(mdStr);
+  function parseMarkdown(mdStr) {
+    return marked(mdStr);
   }
 
-  function processArray(arr, type, parser, MDparser) {
+  //array will only be translated if key defining the array has a TR or MD flag
+  //otherwise keys like layout_body_class or src containing arrays will be put into
+  //translation dict.if the array contains only strings all those strings will be returned
+  //for translation. if it contains an object only those keys with TR|MD will be processed
+  function processArray(arr, type, parser) {
     var reduced =  arr.reduce(function(map, item, index) {
-      if(typeof item === 'object') {
-        map.push(parser(item));
+      var val;
+      if(typeof item === 'object' && item !== null) {
+        val = parser(item);
+      } else if(Array.isArray(item)) {
+        val = processArray(item, type, parser);
+      } else if(type === 'MD') {
+        val = parseMarkdown(item);
       } else {
-        //if it is already know it needs to be translated then push it and return
-        if(type === 'MD') {
-          item = parseMarkdown(item);
-        }
-        map.push(item);
+        val = item;
+      }
+
+      if(val) {
+        map.push(val);
       }
       return map;
     }, []);
@@ -28,7 +37,7 @@ module.exports = function (assemble) {
   }
 
   var createDictionary = function createDictionary(data, locale) {
-    var locales = assemble.get('data.locales');
+    var linkPath = assemble.get('data.linkPath');
 
     if(locales.indexOf(locale) !== -1) {
       linkPath = path.join(linkPath, locale);
@@ -48,28 +57,33 @@ module.exports = function (assemble) {
       if( ( split === 'MD' || split === 'TR' ) && ( data[key] !== 'object' || Array.isArray(data[key]) ) ) {
         switch(split) {
           case 'MD':
+            //if it's an array remember the key
             if(Array.isArray(data[key])) {
             val = processArray(data[key], split, createDictionary);
           } else {
-            val = parseMarkdown(data[key], marked);
+            val = parseMarkdown(data[key]);
           }
           break;
           case 'TR':
+            //if it's an array remember the key
             if(Array.isArray(data[key])) {
-            val = processArray(data[key], split, createDictionary, marked);
+            val = processArray(data[key], split, createDictionary);
           } else {
             val = data[key];
           }
           break;
         }
-      //fucking null....are you kidding me!!!!
-      } else if(typeof data[key] === 'object' && data[key] !== null) {
+        //fucking null....are you kidding me!!!!
+      } else if(typeof data[key] === 'object' && data[key] !== null && !Array.isArray(data[key]) ) {
         recursed = createDictionary(data[key]);
         //this is important, keeps keys with empty values from being added
         if(Object.keys(recursed).length > 0) {
           o[key] = recursed;
         }
       }
+      //else if( Array.isArray(data[key]) ) {
+        //val = processArray(data[key], split, createDictionary);
+      //}
 
       if(val) {
         o[key] = val;
