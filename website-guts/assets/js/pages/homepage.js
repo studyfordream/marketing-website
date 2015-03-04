d6d654b7e5f3bf141f5ad6daa7a3e21ca73a938b
Kyle Rush <kylerrush@gmail.com>
2015-03-03 22:24:28 -0500

    Add form engagement tracking.

--- website-guts/assets/js/pages/homepage.js
+++ website-guts/assets/js/pages/homepage.js
@@ -20,3 +20,59 @@ $('#get-started').submit(function(e){
 
 //deal with placeholder icons
 window.optly.mrkt.anim.placeholderIcons({inputs: $('#get-started input')});
+
+//track focus on form field
+$('#get-started input:not([type="hidden"]), #signup-form input:not([type="hidden"])').each(function(){
+  $(this).one('focus', function(){
+    //put all the information in the event because we'll want to use this as a goal in optimizely
+    w.analytics.track($(this).closest('form').attr('id') + ' ' + $(this).attr('name') + ' focus',
+    {
+      category: 'forms'
+    },
+    {
+      integrations: {
+        'Marketo': false
+      }
+    });
+  });
+});
+
+//track blur on form field
+$('#get-started input:not([type="hidden"]), #signup-form input:not([type="hidden"])').each(function(){
+  $(this).one('blur', function(){
+    //put all the information in the event because we'll want to use this as a goal in optimizely
+    w.analytics.track($(this).closest('form').attr('id') + ' ' + $(this).attr('name') + ' blur',
+    {
+      category: 'forms'
+    },
+    {
+      integrations: {
+        'Marketo': false
+      }
+    });
+  });
+});
+
+//track change on form fields
+$('#get-started input:not([type="hidden"]), #signup-form input:not([type="hidden"])').each(function(){
+  var element = this;
+  var continuallyCheckForValue = setInterval(function(){
+    if($(element).val()){
+      clearInterval(continuallyCheckForValue);
+      w.analytics.track($(element).closest('form').attr('id') + ' ' + $(element).attr('name') + ' value changed', {
+        category: 'forms'
+      },{
+        integrations: {
+          'Marketo': false
+        }
+      });
+      w.analytics.track($(element).closest('form').attr('id') + ' value engagement', {
+        category: 'forms'
+      },{
+          integrations: {
+            'Marketo': false
+          }
+      });
+    }
+  }, 1000);
+});
