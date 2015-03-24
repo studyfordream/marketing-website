a1a4d4ee2272315541ea10d4290e2c08143ec5f4
Stewart Smith <stewart@optimizely.com>
2015-03-24 12:27:32 -0400

    dfp comments -- spacing, css cleanup, yml cleanup

--- website-guts/assets/js/pages/opties.js
+++ website-guts/assets/js/pages/opties.js
@@ -1,5 +1,6 @@
 $('.js-category-cta').on('click', function(event) {
-  var category = ($(this).data().category);
+  var category = $(this).data('category');
+  console.log(category);
   $('#Field1').val(category);
   w.optly.mrkt.utils.smoothScroll.apply(this, [event]);
 });
