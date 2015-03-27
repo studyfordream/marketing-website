0369009c6a695c7a241455cb6497052015d869e0
Stewart Smith <stewart@optimizely.com>
2015-03-27 14:04:37 -0400

    begin filter work

--- website-guts/assets/js/pages/opticon-agenda.js
+++ website-guts/assets/js/pages/opticon-agenda.js
@@ -1,9 +1,26 @@
-var accordianControl = $('.js-accordian-control');
+//Accordian for talks
+var $accordianControls = $('.js-accordian-control');
 $('.js-toggle-cont').slideToggle('fast'); //all details closed to start
-
-$(accordianControl).click(function(event) {
+$accordianControls.click(function(event) {
   event.preventDefault();
   //TODO Find correct selector for this line
   $(this).next().toggleClass('expansion-arrow--open');
   $(this).next().next().slideToggle('fast');
+});
+$('.js-arrow').click(function(event) {
+  $(this).toggleClass('expansion-arrow--open');
+  $(this).next().slideToggle('fast');
+});
+
+
+//Top Filter
+var $dropdownElems = $('.js-dropdown');
+$dropdownElems.click(function(event) {
+  var $this = $(this);
+  $this.toggleClass('active');
+  $dropdownElems.not( $this ).removeClass( 'active' );
+});
+
+$('.js-filter-item').on('click', function(event) {
+  $('body').toggleClass($.trim($(this).attr('data-filter')));
 });
