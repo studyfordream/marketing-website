3101b8a884bc52af9258b228476c7e73cd26d84c
Stewart Smith <stewart@optimizely.com>
2015-04-16 13:45:17 -0400

    clean up modals, add prospectus link, change header and footer

--- website-guts/assets/js/pages/opticon-sponsors.js
+++ website-guts/assets/js/pages/opticon-sponsors.js
@@ -1,7 +1,11 @@
 $('[data-modal-click="opticon-sponsor-modal"]').on('click', function(e) {
   e.preventDefault();
   window.optly.mrkt.modal.open({modalType: 'opticon-sponsor-modal'});
-  $('.js-company-image').css('background-image', this.style.backgroundImage);
-  $('.js-company-name').html(this.dataset.name);
+  $('.js-company-image')
+    .css('background-image', this.style.backgroundImage)
+    .attr('href', this.dataset.link);
+  $('.js-company-name')
+    .html(this.dataset.name)
+    .attr('href', this.dataset.link);
   $('.js-company-description').html(this.dataset.description);
 });
