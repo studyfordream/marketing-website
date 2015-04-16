6d490174bbffb0bcffc4e5e4d7cc128c15bf0e73
Stewart Smith <stewart@optimizely.com>
2015-04-16 12:46:03 -0400

    modal working with styling, touchups needed

--- website-guts/assets/js/pages/opticon-sponsors.js
+++ website-guts/assets/js/pages/opticon-sponsors.js
@@ -1,4 +1,7 @@
 $('[data-modal-click="opticon-sponsor-modal"]').on('click', function(e) {
   e.preventDefault();
   window.optly.mrkt.modal.open({modalType: 'opticon-sponsor-modal'});
+  $('.js-company-image').css('background-image', this.style.backgroundImage);
+  $('.js-company-name').html(this.dataset.name);
+  $('.js-company-description').html(this.dataset.description);
 });
