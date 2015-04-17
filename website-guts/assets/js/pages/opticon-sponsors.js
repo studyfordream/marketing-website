23cd37327777bbc9154179cb9953b10fbd1ae5de
Stewart Smith <stewart@optimizely.com>
2015-04-17 10:26:12 -0400

    add segment, roboboogie, take out dataset for IE

--- website-guts/assets/js/pages/opticon-sponsors.js
+++ website-guts/assets/js/pages/opticon-sponsors.js
@@ -1,11 +1,7 @@
 $('[data-modal-click="opticon-sponsor-modal"]').on('click', function(e) {
   e.preventDefault();
   window.optly.mrkt.modal.open({modalType: 'opticon-sponsor-modal'});
-  $('.js-company-image')
-    .css('background-image', this.style.backgroundImage)
-    .attr('href', this.dataset.link);
-  $('.js-company-name')
-    .html(this.dataset.name)
-    .attr('href', this.dataset.link);
+  $('.js-company-image').css('background-image', this.style.backgroundImage).attr('href', this.getAttribute('data-link'));
+  $('.js-company-name').html(this.getAttribute('data-name')).attr('href', this.dataset.link);
   $('.js-company-description').html(this.dataset.description);
 });
