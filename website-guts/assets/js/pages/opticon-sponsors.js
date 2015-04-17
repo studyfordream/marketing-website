06f5d3b85ebd2b4b093ac79ba7d9fff48265f19b
Stewart Smith <stewart@optimizely.com>
2015-04-17 10:41:18 -0400

    change linter, clean js and css

--- website-guts/assets/js/pages/opticon-sponsors.js
+++ website-guts/assets/js/pages/opticon-sponsors.js
@@ -2,6 +2,6 @@ $('[data-modal-click="opticon-sponsor-modal"]').on('click', function(e) {
   e.preventDefault();
   window.optly.mrkt.modal.open({modalType: 'opticon-sponsor-modal'});
   $('.js-company-image').css('background-image', this.style.backgroundImage).attr('href', this.getAttribute('data-link'));
-  $('.js-company-name').html(this.getAttribute('data-name')).attr('href', this.dataset.link);
-  $('.js-company-description').html(this.dataset.description);
+  $('.js-company-name').html(this.getAttribute('data-name')).attr('href', this.getAttribute('data-link'));
+  $('.js-company-description').html(this.getAttribute('data-description'));
 });
