9c3865d2b3b8f7aeef8373500278c5f985730cb4
Stewart Smith <stewart@optimizely.com>
2015-04-22 17:22:48 -0400

    add speaker modal and style it and javascript

--- website-guts/assets/js/pages/opticon-speakers.js
+++ website-guts/assets/js/pages/opticon-speakers.js
@@ -1,10 +1,10 @@
 $('.js-speaker-info').on('click', function(e) {
-  console.log('ham');
   e.preventDefault();
   window.optly.mrkt.modal.open({modalType: 'opticon-sponsor-modal'});
-  $('.js-company-image').css('background-image', this.getAttribute('data-image'));
-  $('.js-company-name').html(this.getAttribute('data-name'));
-  $('.js-company-description').html(this.getAttribute('data-bio'));
+  $('.js-speaker-image').css('background-image', this.getAttribute('data-image'));
+  $('.js-speaker-name').html(this.getAttribute('data-name'));
+  $('.js-speaker-company').html(this.getAttribute('data-company'));
+  $('.js-speaker-description').html(this.getAttribute('data-bio'));
 });
 
 $('.js-close').on('click', function() {
