9f276a0f212e79542ce4ddd188e67e1021932f5b
David Fox-Powell <dtothefp@gmail.com>
2015-05-01 18:43:31 -0400

    add importer and image-url function

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,18 +1,20 @@
 var path = require('path');
+var fs = require('fs');
 
-module.exports = {
-  prod: {
-    options: {
-      sourceMap: false,
-      imagePath: '<%= grunt.config.get("sassImagePath") %>',
-      precision: 3,
-      outputStyle: 'compressed',
-      includePaths: [
-        path.join(process.cwd(), 'node_modules/css-smart-grid/sass')
-      ]
-    },
-    files: [
-      {
+module.exports = function(grunt){
+  return {
+    prod: {
+      options: {
+        sourceMap: false,
+        imagePath: '<%= grunt.config.get("sassImagePath") %>',
+        precision: 3,
+        outputStyle: 'compressed',
+        includePaths: [
+          path.join(process.cwd(), 'node_modules/css-smart-grid/sass')
+        ]
+      },
+      files: [
+        {
         src: '<%= config.guts %>/assets/css/styles.scss',
         dest: '<%= config.temp %>/css/styles.css'
       },
@@ -20,19 +22,42 @@ module.exports = {
         src: '<%= config.guts %>/assets/css/om/styles.scss',
         dest: '<%= config.temp %>/css/om/styles.css'
       }
-    ]
-  },
-  dev: {
-    options: {
-      sourceMap: false,
-      imagePath: '<%= grunt.config.get("sassImagePath") %>',
-      precision: 3,
-      includePaths: [
-        path.join(process.cwd(), 'node_modules/css-smart-grid/sass')
       ]
     },
-    files: [
-      {
+    dev: {
+      options: {
+        sourceMap: false,
+        imagePath: '<%= grunt.config.get("sassImagePath") %>',
+        precision: 3,
+        includePaths: [
+          path.join(process.cwd(), 'node_modules/css-smart-grid/sass')
+        ],
+        importer: function(url, prev, done) {
+          var fp = path.join(path.dirname(prev), url + '.scss');
+          var env, contents, imageUrl;
+          //console.log('URL', url);
+          //console.log('PREV', prev);
+
+          if(/variables/.test(url)) {
+            imageUrl = grunt.config.get('imageUrl');
+            env = grunt.config.get('environment');
+            contents = fs.readFileSync(fp, {encoding: 'utf8'});
+            contents = '$imageUrl: ' + imageUrl+ ';\n\nbody {background: red}';
+            return {contents: contents};
+          } else {
+            if(url = 'smart-grid') {
+              console.log(url);
+              //url = path.join(process.cwd(), 'node_modules/css-smart-grid/sass', url);
+              url = path.join('smart-grid', url + '.scss');
+              console.log(url);
+            }
+            return {file: url};
+          }
+
+        }
+      },
+      files: [
+        {
         src: '<%= config.guts %>/assets/css/styles.scss',
         dest: '<%= config.temp %>/css/styles.css'
       },
@@ -40,6 +65,8 @@ module.exports = {
         src: '<%= config.guts %>/assets/css/om/styles.scss',
         dest: '<%= config.temp %>/css/om/styles.css'
       }
-    ]
-  }
+      ]
+    }
+
+  };
 };
