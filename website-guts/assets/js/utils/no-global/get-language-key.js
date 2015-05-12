9b109bb9adb8d8c46f43c778767f91055cdc1e33
David Fox-Powell <dtothefp@gmail.com>
2015-05-12 11:30:18 -0700

    add locale tracking data to oform middleware

---
+++ website-guts/assets/js/utils/no-global/get-language-key.js
@@ -0,0 +1,26 @@
+/**
+ * Utility function to append locale specific data to oForm payload object sent to /account/create endpoint
+ *
+ * @param {String} `data` string of form data encoded by oForm
+ * @return {String} encoded locale string to be appended to the oForm `data` string in the oForm middleware
+ */
+
+module.exports = function(data) {
+  var langKey;
+  var tld = window.location.hostname.split('.').pop();
+
+  if(window.optly.l10n && window.optly.l10n.locales) {
+    for(var key in window.optly.l10n.locales) {
+      if(key === tld) {
+        langKey = window.optly.l10n.locales[key];
+        break;
+      }
+    }
+
+    if(!langKey) {
+      langKey = 'us_EN';
+    }
+  }
+
+  return langKey ? window.encodeURI('&locale=' + langKey) : '';
+};
