c0aacc324c60793e721ea46b1ffb6a1e7e0cf47d
Daniel O'Connor <daniel@optimizely.com>
2015-05-19 20:53:04 -0700

    Implement SSO.

--- website-guts/assets/js/components/modal-signin.js
+++ website-guts/assets/js/components/modal-signin.js
@@ -1,40 +1,71 @@
 var signinDialogHelperInst = window.optly.mrkt.form.signin({formId: 'signin-form'});
+var signinFormSelector = '#signin-form';
 
-var signinForm = new Oform({
-  selector: '#signin-form',
-  customValidation: {
-    password: function(elm) {
-      return signinDialogHelperInst.passwordValidation(elm);
-    }
-  },
-  middleware: w.optly.mrkt.Oform.defaultMiddleware
-});
+var signinFormEmail = $('#signin-form-email');
+var signinFormPasswordLabel = $('#signin-form-password-label');
+var signinFormSSOCheckbox = $('#signin-form-sso-checkbox');
+var isSSOSubmission = signinFormSSOCheckbox.checked;
 
-signinForm.on('before', function() {
-  signinDialogHelperInst.processingAdd();
-  return true;
-});
+$(signinFormSelector).on('submit', function(event) {
+  event.preventDefault();
 
-signinForm.on('validationerror', w.optly.mrkt.Oform.validationError);
+  if (isSSOSubmission) {
+    // Login with single sign-on
+    $.ajax({
+      type: 'POST',
+      // Change this to use a global variable for the prefix
+      url: '//app.optimizely.com' + '/sp_initiated_signin',
+      data: {
+        email: signinFormEmail.val()
+      },
+      xhrFields: {
+        withCredentials: true
+      }
+    });
+  } else {
+    // Login with username and password
+    var signinForm = new Oform({
+      selector: signinFormSelector,
+      customValidation: {
+        password: function(elm) {
+          return signinDialogHelperInst.passwordValidation(elm);
+        }
+      },
+      middleware: w.optly.mrkt.Oform.defaultMiddleware
+    });
 
-signinForm.on('error', function() {
-  signinDialogHelperInst.processingRemove({callee: 'error'});
-  signinDialogHelperInst.showOptionsError({error: 'UNEXPECTED'});
-  window.analytics.track('signin xhr error', {
-    category: 'account',
-    label: w.location.pathname
-  }, {
-    integrations: {
-      Marketo: false
-    }
-  });
-}.bind(signinDialogHelperInst));
+    signinForm.on('before', function() {
+      signinDialogHelperInst.processingAdd();
+      return true;
+    });
 
-signinForm.on('load', signinDialogHelperInst.load.bind(signinDialogHelperInst));
+    signinForm.on('validationerror', w.optly.mrkt.Oform.validationError);
 
-signinForm.on('done', function(){
-  signinDialogHelperInst.processingRemove({callee: 'done'});
-  if (document.body.classList.contains('oform-error')) {
-    signinDialogHelperInst.showOptionsError();
+    signinForm.on('error', function() {
+      signinDialogHelperInst.processingRemove({callee: 'error'});
+      signinDialogHelperInst.showOptionsError({error: 'UNEXPECTED'});
+      window.analytics.track('signin xhr error', {
+        category: 'account',
+        label: w.location.pathname
+      }, {
+        integrations: {
+          Marketo: false
+        }
+      });
+    }.bind(signinDialogHelperInst));
+
+    signinForm.on('load', signinDialogHelperInst.load.bind(signinDialogHelperInst));
+
+    signinForm.on('done', function(){
+      signinDialogHelperInst.processingRemove({callee: 'done'});
+      if (document.body.classList.contains('oform-error')) {
+        signinDialogHelperInst.showOptionsError();
+      }
+    }.bind(signinDialogHelperInst));
   }
-}.bind(signinDialogHelperInst));
+});
+
+signinFormSSOCheckbox.change(function() {
+  isSSOSubmission = this.checked;
+  signinFormPasswordLabel.toggle(!isSSOSubmission);
+});
